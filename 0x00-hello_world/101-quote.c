8c249b17478477010914d419e4997fc9b96ae647
rahman sanuth <sanuthrahman@gmail.com>
2022-10-13 11:22:56 +0100

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,10 @@
 #include <unistd.h>
 
+/**
+ *main - entry point
+ *
+ *Return: 1 after printing the function
+ */
 int main(void)
 {
 write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59);
