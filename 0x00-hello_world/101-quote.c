4a36e2d39204fb20f941b1bbee180b7128bd43e1
rahman sanuth <sanuthrahman@gmail.com>
2022-10-13 11:29:47 +0100

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,4 @@
-#include <unistd.h>
-
+#include<unistd.h>
 /**
  * main - prints the standrad error sandwiched around the  standard outpu/t
  *
@@ -7,6 +6,6 @@
  */
 int main(void)
 {
-write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-return (1);  
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
 }
