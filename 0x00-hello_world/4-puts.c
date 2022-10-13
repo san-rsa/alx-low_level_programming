d802a733a8dabcd6cb1947e36d4e1d0bac57ccbf
rahman sanuth <sanuthrahman@gmail.com>
2022-10-13 11:14:46 +0100

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 /**
-*main entry
-return 0
-*/
+ *main - start 
+ *
+ *Return: 0 after printing the function
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
