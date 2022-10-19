8cad5b1d05a6bef3c68b6214a4add6a3588fb5c3
rahman sanuth <sanuthrahman@gmail.com>
2022-10-19 12:34:01 +0100

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,7 +8,7 @@
 int main(void)
 {
 _putchar('_');
-_putchar('p');  
+_putchar('p');
 _putchar('u');
 _putchar('t');
 _putchar('c');
@@ -17,5 +17,5 @@ _putchar('a');
 _putchar('r');
 _putchar('\n');
 
-return 0;
+return (0);
 }
