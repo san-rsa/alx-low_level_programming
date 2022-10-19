e27279485545f4e7c4102a0dad883aa7fcc5ad23
rahman sanuth <sanuthrahman@gmail.com>
2022-10-19 12:40:22 +0100

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * print_alphabet - print alphabet lowercase
+ * print_alphabet_x10 - print alphabet
  *
  * Return: Always 0.
  */
 
 void print_alphabet_x10(void)
 {
-int alphabet;  
+int alphabet;
 int count;
 
 count = 0;
